43e9633bf4291721f77f91ad612c2ca3a406f29c
Vladimir Svetlakov <vova.svetl@mail.ru>
2022-12-11 19:29:01 +0300

    Array is being read from user's input

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int[] list = {1,2,3};
-        System.out.println(max(list));
+        Scanner input = new Scanner(System.in);
+        int n,k;
+        System.out.println("Введите количество элементов массива");
+        n = input.nextInt();
+        int[] list = new int[n];
+        System.out.println("Введите элементы массива по одному");
+        for (int i=0; i<n; i++){
+            k=input.nextInt();
+            list[i]=k;
+        }
+        System.out.println("Максимум массива = "+max(list));
     }
     public static int max(int[] obj){
         int m= obj[0];
