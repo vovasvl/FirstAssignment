71a82119832df1ff58b14cc3a978f2ebe9ac0c17
Vladimir Svetlakov <vova.svetl@mail.ru>
2022-12-11 16:26:31 +0300

    Added method "max"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] list = {1,2,3};
+        System.out.println(max(list));
+    }
+    public static int max(int[] obj){
+        int m= obj[0];
+        for (int i =0; i<obj.length; i++){
+            if(obj[i]>m){
+                m=obj[i];
+            }
+        }
+        return m;
     }
 }
